fadfaa72e0c6a474e5b3fc8948399ac038f7dbbc
VoidOfMaya <mayaofthevoid@gmail.com>
2026-01-06 15:41:47 +0200

    feat(index.js)implements basic server with routing  and pages

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 import http from 'http'
+import fs from "fs"
 
+// lauching server
 const server = http.createServer((req, res)=>{
+    //declaring res content type
+    res.setHeader('Content-Type', 'text/html');
 
+    //pages path
+    let path ='./pages/'
+
+    //router
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break;
+        
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break;
+
+        case '/contact-me':
+            path += 'contact-me.html'
+            res.statusCode = 200
+            break;
+        // 404 handelling
+        default:
+            path+= '404.html'
+            res.statusCode= 404
+            break;
+    }
+    //
+    fs.readFile(path, (err, data)=>{
+        err ? console.log(err) : res.end(data);
+    })
 })
-server.listen(8080);
+// server listining on localhost, port 8080
+server.listen(8080, 'localhost',()=>{
+    console.log('server running...');
+});
